5f6edb756bd966b82a4a6a9e312e30664405ad29
TOM SAJU <tomsaju33@gmail.com>
2022-05-27 00:12:03 +0530

    Rendering DB items to the todolist

--- app.js
+++ app.js
@@ -4,11 +4,15 @@ const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 //mongoose connection
-mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true
+});
 
 //Schema creation
 const itemsSchema = {
@@ -36,19 +40,11 @@ const item3 = new Item({
 //saving it in an array so as to use insertMany() method instead of insertOne()
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(error){
-  if(error){
-    console.log(error);
-  }else{
-    console.log("Successfully saved the default items!!");
-  }
-});
-
 //version 1 storage!
 // var items = [];
 // var workItems = [];
 
-app.get("/", function(request, response){
+app.get("/", function(request, response) {
   var today = new Date();
 
   var options = {
@@ -56,27 +52,48 @@ app.get("/", function(request, response){
     day: "numeric",
     month: "long"
   };
-
   var day = today.toLocaleDateString("en-US", options);
-  response.render("list", {listTitle: day, newListItems: items});
+
+  //Find items from // DEBUG:
+  Item.find({}, function(error, foundItems) {
+
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(error) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Successfully saved the default items!!");
+        }
+      });
+      response.redirect("/");
+    } else {
+      response.render("list", {
+        listTitle: day,
+        newListItems: foundItems
+      });
+    }
+  });
 });
 
-app.post("/", function(request, response){
+app.post("/", function(request, response) {
   var item = request.body.newItem;
 
-    if(request.body.list === "Work List"){
-        workItems.push(item);
-        response.redirect("/work");
-    }else{
-        items.push(item);
-        response.redirect("/");
-    }
+  if (request.body.list === "Work List") {
+    workItems.push(item);
+    response.redirect("/work");
+  } else {
+    items.push(item);
+    response.redirect("/");
+  }
 });
 
-app.get("/work", function(request, response){
-  response.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function(request, response) {
+  response.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
-app.listen(3000, function(){
+app.listen(3000, function() {
   console.log("Server is running on port 3000!");
 });
