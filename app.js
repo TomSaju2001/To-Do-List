a9d2d655b3ace5b19a9c5f2551260df1ff06be76
TOM SAJU <tomsaju33@gmail.com>
2022-05-26 22:52:30 +0530

    Added mongo db connection and inserted few default items todo list

--- app.js
+++ app.js
@@ -1,13 +1,52 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-var items = [];
-var workItems = [];
+//mongoose connection
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+//Schema creation
+const itemsSchema = {
+  name: {
+    type: String
+  }
+}
+
+//Model Creation
+const Item = mongoose.model("Item", itemsSchema);
+
+//Data1
+const item1 = new Item({
+  name: "Welcome to your todolist!!"
+});
+
+const item2 = new Item({
+  name: "Hit the + button to add new item."
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+//saving it in an array so as to use insertMany() method instead of insertOne()
+const defaultItems = [item1, item2, item3];
+
+Item.insertMany(defaultItems, function(error){
+  if(error){
+    console.log(error);
+  }else{
+    console.log("Successfully saved the default items!!");
+  }
+});
+
+//version 1 storage!
+// var items = [];
+// var workItems = [];
 
 app.get("/", function(request, response){
   var today = new Date();
